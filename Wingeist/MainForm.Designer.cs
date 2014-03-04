a1d15bacfed7e4a1e785a106644453ad43a06d3f
wesley Bliss <wesley.bliss@gmail.com>
2014-03-04 13:19:47 -0500

    UI - stats labels

--- Wingeist/MainForm.Designer.cs
+++ Wingeist/MainForm.Designer.cs
@@ -29,7 +29,12 @@
             this.menuContextExit = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelEventsToday = new System.Windows.Forms.Label();
+            this.labelEventsThisWeek = new System.Windows.Forms.Label();
             this.contextMenuMain.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // contextMenuMain
@@ -56,20 +61,62 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.labelEventsThisWeek);
+            this.panel1.Controls.Add(this.labelEventsToday);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(706, 100);
+            this.panel1.Size = new System.Drawing.Size(706, 77);
             this.panel1.TabIndex = 1;
             // 
             // panel2
             // 
             this.panel2.AutoSize = true;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Location = new System.Drawing.Point(0, 77);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(706, 573);
+            this.panel2.Size = new System.Drawing.Size(706, 596);
             this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Events Today:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Events This Week:";
+            // 
+            // labelEventsToday
+            // 
+            this.labelEventsToday.AutoSize = true;
+            this.labelEventsToday.Location = new System.Drawing.Point(167, 9);
+            this.labelEventsToday.Name = "labelEventsToday";
+            this.labelEventsToday.Size = new System.Drawing.Size(19, 23);
+            this.labelEventsToday.TabIndex = 3;
+            this.labelEventsToday.Text = "0";
+            // 
+            // labelEventsThisWeek
+            // 
+            this.labelEventsThisWeek.AutoSize = true;
+            this.labelEventsThisWeek.Location = new System.Drawing.Point(167, 41);
+            this.labelEventsThisWeek.Name = "labelEventsThisWeek";
+            this.labelEventsThisWeek.Size = new System.Drawing.Size(19, 23);
+            this.labelEventsThisWeek.TabIndex = 4;
+            this.labelEventsThisWeek.Text = "0";
             // 
             // MainForm
             // 
@@ -85,6 +132,8 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.contextMenuMain.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,6 +146,10 @@
         private System.Windows.Forms.ToolStripMenuItem menuContextPauseResume;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelEventsThisWeek;
+        private System.Windows.Forms.Label labelEventsToday;
     }
 }
 
