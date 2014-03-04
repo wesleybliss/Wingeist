d6227d4931154c3a84fa37bd0c3082c2a44b44f4
wesley Bliss <wesley.bliss@gmail.com>
2014-03-04 13:06:41 -0500

    Added system tray icon + context menu.

---
+++ Wingeist/MainForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Wingeist {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuContextExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuContextPauseResume = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuMain
+            // 
+            this.contextMenuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuContextPauseResume,
+            this.menuContextExit});
+            this.contextMenuMain.Name = "contextMenuMain";
+            this.contextMenuMain.Size = new System.Drawing.Size(176, 80);
+            // 
+            // menuContextExit
+            // 
+            this.menuContextExit.Name = "menuContextExit";
+            this.menuContextExit.Size = new System.Drawing.Size(175, 24);
+            this.menuContextExit.Text = "E&xit";
+            this.menuContextExit.Click += new System.EventHandler(this.menuContextExit_Click);
+            // 
+            // menuContextPauseResume
+            // 
+            this.menuContextPauseResume.Name = "menuContextPauseResume";
+            this.menuContextPauseResume.Size = new System.Drawing.Size(175, 24);
+            this.menuContextPauseResume.Text = "&Pause";
+            this.menuContextPauseResume.Click += new System.EventHandler(this.menuContextPauseResume_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 673);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "Wingeist";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.contextMenuMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuMain;
+        private System.Windows.Forms.ToolStripMenuItem menuContextExit;
+        private System.Windows.Forms.ToolStripMenuItem menuContextPauseResume;
+    }
+}
+
---
+++ Wingeist/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Wingeist {
+
+    public partial class MainForm : Form {
+
+        private NotifyIcon notifyIcon;
+        private FileSystemWatcher fsw;
+
+        public MainForm() {
+            InitializeComponent();
+            Application.ApplicationExit += Application_ApplicationExit;
+        }
+
+        void Application_ApplicationExit( object sender, EventArgs e ) {
+            // Release the icon resource.
+            notifyIcon.Dispose();
+        }
+
+        private void MainForm_Load( object sender, EventArgs e ) {
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Text = "Wingeist";
+            notifyIcon.Icon = new Icon( SystemIcons.Asterisk, 40, 40 );
+            notifyIcon.ContextMenuStrip = contextMenuMain;
+            notifyIcon.Click += notifyIcon_Click;
+            notifyIcon.DoubleClick += notifyIcon_DoubleClick;
+            notifyIcon.Visible = false;
+
+            fsw = new FileSystemWatcher( @"C:\Users\wes", "*.txt" );
+            fsw.NotifyFilter = NotifyFilters.LastAccess |
+                         NotifyFilters.LastWrite |
+                         NotifyFilters.FileName |
+                         NotifyFilters.DirectoryName;
+            fsw.IncludeSubdirectories = true;
+            fsw.Created += new FileSystemEventHandler( fsw_Created );
+            fsw.EnableRaisingEvents = true;
+
+        }
+
+        private void MainForm_FormClosing( object sender, FormClosingEventArgs e ) {
+            e.Cancel = true;
+            this.Hide();
+            this.ShowInTaskbar = false;
+            notifyIcon.Visible = true;
+        }
+
+        void notifyIcon_Click( object sender, EventArgs e ) {
+            contextMenuMain.Show( MousePosition );
+        }
+
+        void notifyIcon_DoubleClick( object sender, EventArgs e ) {
+            this.ShowInTaskbar = true;
+            this.Show();
+            notifyIcon.Visible = false;
+        }
+
+        private void menuContextPauseResume_Click( object sender, EventArgs e ) {
+
+        }
+
+        private void menuContextExit_Click( object sender, EventArgs e ) {
+            Application.Exit();
+        }
+
+        private void fsw_Created( object sender, FileSystemEventArgs e ) {
+            //this.Invoke( (MethodInvoker) delegate {
+            //    listBox1.Items.Add( e.Name + " ... " + e.FullPath );
+            //} );
+        }
+
+    } // class
+
+} // ns
