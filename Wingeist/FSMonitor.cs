770d4dbea1f72a4081a6a5bc65cf2072f53af95d
wesley Bliss <wesley.bliss@gmail.com>
2014-03-04 21:13:36 -0500

    Commented some of the main methods.

---
+++ Wingeist/FSMonitor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Wingeist {
+
+    class FSMonitor {
+
+        private FileSystemWatcher fsw;
+        private Form parent;
+        private string monitorPath;
+
+        public FSMonitor( Form parentForm, string monitorPath ) {
+
+            this.parent = parentForm;
+            this.monitorPath = monitorPath;
+
+            fsw = new FileSystemWatcher( monitorPath, "*" );
+            fsw.NotifyFilter = NotifyFilters.LastAccess |
+                         NotifyFilters.LastWrite |
+                         NotifyFilters.FileName |
+                         NotifyFilters.DirectoryName;
+            fsw.IncludeSubdirectories = true;
+            fsw.Created += new FileSystemEventHandler( fsw_Created );
+
+        }
+
+        private void fsw_Created( object sender, FileSystemEventArgs e ) {
+            //parent.Invoke( (MethodInvoker) delegate {
+            //    listEvents.Items.Add( e.Name + " ... " + e.FullPath );
+            //} );
+            parent.Invoke( parent.addFSEventDelegate );
+        }
+
+    }
+
+}
--- Wingeist/MainForm.cs
+++ Wingeist/MainForm.cs
@@ -13,28 +13,47 @@ namespace Wingeist {
 
     public partial class MainForm : Form {
 
+        // Helper class to manage settings
         private SettingsHelper settingsHelper;
 
+        // Keys for image list for swapping button visuals
         private static string IMAGE_PLAY_KEY = "play";
         private static string IMAGE_PAUSED_KEY = "pause";
 
+        // Tray icon support
         private NotifyIcon notifyIcon;
+
+        // Monitoring local file system
         private FileSystemWatcher fsw;
+
+        // Allow pause/resume of watching
         private bool isPaused = true;
 
+        /// <summary>
+        /// Class constructor
+        /// </summary>
         public MainForm() {
             InitializeComponent();
+            // Catch application exit to free resources
             Application.ApplicationExit += Application_ApplicationExit;
+            // Get an instance of settings helper
             settingsHelper = new SettingsHelper();
         }
 
+        /// <summary>
+        /// Catches application exit to free resources
+        /// </summary>
         void Application_ApplicationExit( object sender, EventArgs e ) {
             // Release the icon resource.
             notifyIcon.Dispose();
         }
 
+        /// <summary>
+        /// Main form load - setup tray icon & FS monitor
+        /// </summary>
         private void MainForm_Load( object sender, EventArgs e ) {
 
+            // Tray icon setup
             notifyIcon = new NotifyIcon();
             notifyIcon.Text = "Wingeist";
             notifyIcon.Icon = new Icon( SystemIcons.Asterisk, 40, 40 );
@@ -43,57 +62,95 @@ namespace Wingeist {
             notifyIcon.DoubleClick += notifyIcon_DoubleClick;
             notifyIcon.Visible = false;
 
+            // Setup FS monitor but don't auto-start (@todo setting for auto-start)
             fsw = new FileSystemWatcher( settingsHelper.monitorPath, "*" );
             fsw.NotifyFilter = NotifyFilters.LastAccess |
                          NotifyFilters.LastWrite |
                          NotifyFilters.FileName |
                          NotifyFilters.DirectoryName;
             fsw.IncludeSubdirectories = true;
+            // @todo More handlers? with filters?
             fsw.Created += new FileSystemEventHandler( fsw_Created );
 
         }
 
+        /// <summary>
+        /// Catch form close event but don't allow application to exit.
+        /// Instead, show tray icon.
+        /// </summary>
         private void MainForm_FormClosing( object sender, FormClosingEventArgs e ) {
+            // Prevent application from exiting
             e.Cancel = true;
+            // Hide all form components
             this.Hide();
             this.ShowInTaskbar = false;
+            // Show tray icon
             notifyIcon.Visible = true;
         }
 
+        /// <summary>
+        /// Show context menu (same as right-click menu for now)
+        /// </summary>
         void notifyIcon_Click( object sender, EventArgs e ) {
             contextMenuMain.Show( MousePosition );
         }
 
+        /// <summary>
+        /// Show main form and hide tray icon
+        /// </summary>
         void notifyIcon_DoubleClick( object sender, EventArgs e ) {
             this.ShowInTaskbar = true;
             this.Show();
             notifyIcon.Visible = false;
         }
 
+        /// <summary>
+        /// Pause or resume watching the local file system
+        /// </summary>
         private void menuContextPauseResume_Click( object sender, EventArgs e ) {
             swapMode();
         }
 
+        /// <summary>
+        /// Allow exiting the application from the tray menu
+        /// </summary>
         private void menuContextExit_Click( object sender, EventArgs e ) {
             Application.Exit();
         }
 
+        /// <summary>
+        /// Handle file created events
+        /// </summary>
         private void fsw_Created( object sender, FileSystemEventArgs e ) {
+            // Add the created file to the list
+            // Invoke required as this is on a different thread
             this.Invoke( (MethodInvoker) delegate {
                 listEvents.Items.Add( e.Name + " ... " + e.FullPath );
             } );
         }
 
+        /// <summary>
+        /// Switch between paused and running modes of monitoring
+        /// </summary>
         private void swapMode() {
+            // Track current state
             isPaused = !isPaused;
             fsw.EnableRaisingEvents = !isPaused;
+            // Update UI
             buttonPauseResume.ImageKey = isPaused ? IMAGE_PLAY_KEY : IMAGE_PAUSED_KEY;
+            menuContextPauseResume.Text = isPaused ? "&Resume" : "&Pause";
         }
 
+        /// <summary>
+        /// Allow mode switching from tray icon
+        /// </summary>
         private void buttonPauseResume_Click( object sender, EventArgs e ) {
             swapMode();
         }
 
+        /// <summary>
+        /// Show settings window (dialog)
+        /// </summary>
         private void buttonSettings_Click( object sender, EventArgs e ) {
             ( new SettingsForm() ).ShowDialog( this );
         }
